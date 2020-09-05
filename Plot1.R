bc6174e2dbffd8e615d8d2637aeb0f23dbe9b5cb
JorgeCarrillo93 <31869334+JorgeCarrillo93@users.noreply.github.com>
2020-09-04 19:59:57 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,8 +1,7 @@
-data <- read.csv("./household_power_consumption.txt", 
-                 sep = ";", header = TRUE, 
-                 colClasses = c('character', 'charachter','numeric', 'numeric',
-                                'numeric','numeric','numeric','numeric','numeric')
-                 , na.strings = "?")
+data <- read.csv("./household_power_consumption.txt", sep = ";", header= TRUE,
+                 colClasses = c("character", "character",
+                                "numeric", "numeric", "numeric", "numeric",
+                                "numeric", "numeric", "numeric"), na.strings = "?" )
 
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
@@ -10,9 +9,12 @@ x <- subset(data, Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
 
 x$dateTime <- as.POSIXct(dateTime)
 
-hist(x$Global_active_power, col = "red", main = "Global Active Power", 
-     xlab = "Global Active Power (kilowatts)")
+x <- x[complete.cases(x),]
+
+hist(x$Global_active_power, main="Global Active Power",
+     xlab = "Global Active Power (kilowatts)", col="red")
 
 dev.copy(png, "plot1.png", width = 480, height = 480)
 
 dev.off()
+
